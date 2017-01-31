055796cd3911d3c7e9f40f38066197fff3cbee6b
Kevin Bee <kevin.bee@uni-ulm.de>
2017-01-31 15:57:28 +0100

    Swipe Direction added

--- wear/src/main/java/mmci/mmcifocusedswipeboard/MainActivity.java
+++ wear/src/main/java/mmci/mmcifocusedswipeboard/MainActivity.java
@@ -4,14 +4,23 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.wearable.view.WatchViewStub;
 import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnTouchListener {
 
     private TextView mTextView;
     private static EditText text;
+    private View testView;
+
+    private float x1,x2,y1,y2;
+    static final int MIN_DISTANCE = 30;
+    private int charBlock;
+    private int swipeDirection;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,12 +35,93 @@ public class MainActivity extends Activity {
         });
         setContentView(R.layout.rect_activity_main);
         text = (EditText) findViewById(R.id.editText);
-        text.setOnClickListener(new View.OnClickListener() {
+       text.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //TODO Wenn auf das Textfeld geklickt wird, soll die View/der Fokus geändert werden
                 Log.d("Debug", "onClick performed");
-            }
+           }
         });
+        testView = findViewById(R.id.testView);
+        testView.setOnTouchListener(this);
+
+    }
+
+    @Override
+    public boolean onTouch(View view, MotionEvent event) {
+
+
+
+        switch(event.getAction())
+        {
+            case MotionEvent.ACTION_DOWN:
+                x1 = event.getX();
+                y1 = event.getY();
+                Log.d("Debug", "Action_down x1:" + x1 + " y1: " + y1);
+                break;
+            case MotionEvent.ACTION_UP:
+                x2 = event.getX();
+                y2 = event.getY();
+                Log.d("Debug", "Action_up x2:" + x2 + " y2: " + y2);
+                float deltaX = x2 - x1;
+                float deltaY = y2 - y1;
+
+
+                float absDeltaX = Math.abs(deltaX);
+                float absDeltaY = Math.abs(deltaY);
+
+                Log.d("Debug", "DeltaX: " + absDeltaX + " DeltaY: " + absDeltaY);
+
+                //horizontal swipe
+                if (absDeltaX > MIN_DISTANCE && absDeltaY < MIN_DISTANCE){
+                    if (x1 < x2){
+                        //left to right swipe
+                        swipeDirection = 5;
+                    } else {
+                        //right to left swipe
+                        swipeDirection = 1;
+                    }
+                }
+
+                //vertical swipe
+                if (absDeltaX < MIN_DISTANCE && absDeltaY > MIN_DISTANCE){
+                    if (y1 < y2){
+                        //top to bottom swipe
+                        swipeDirection = 7;
+                    } else {
+                        //bottom to top swipe
+                        swipeDirection = 3;
+                    }
+                }
+
+                //corner swipe
+                if (absDeltaX > MIN_DISTANCE && absDeltaY > MIN_DISTANCE){
+                    if (x1 > x2 && y1 < y2) {
+                        //top right to bottom left swipe
+                        swipeDirection = 8;
+                    }
+                    if (x1 > x2 && y1 > y2) {
+                        //bottom right to top left swipe
+                        swipeDirection = 2;
+                    }
+                    if (x1 < x2 && y1 < y2) {
+                        //top left to bottom right swipe
+                        swipeDirection = 6;
+                    }
+                    if (x1 < x2 && y1 > y2) {
+                        //bottom left to top right swipe
+                        swipeDirection = 4;
+                    }
+                }
+
+                // no swipe
+                if (absDeltaX < MIN_DISTANCE && absDeltaY < MIN_DISTANCE){
+                    swipeDirection = 0;
+                }
+
+                Log.d("Debug", "swipe direction: " + swipeDirection);
+                break;
+        }
+        return true;
     }
 }
